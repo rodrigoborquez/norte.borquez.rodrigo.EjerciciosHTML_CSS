5787802e566ca2c2bac2fc944b9218b82d015ea3
rborquez <rborquez@cecaitra.org.ar>
2020-04-28 19:42:43 -0300

    Creación de la carpeta para ir realizando las tareas asignadas por el profesor.

---
+++ SolutionLPPA/LPPA.WebSite/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LPPA.WebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+       
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Ejercicio1()
+        {
+            return View();
+        }
+
+        public ActionResult Ejercicio2()
+        {
+            return View();
+        }
+
+        public ActionResult Ejercicio3()
+        {
+            return View();
+        }
+
+        public ActionResult Ejercicio4()
+        {
+            return View();
+        }
+
+        public ActionResult Ejercicio5()
+        {
+            return View();
+        }
+
+        public ActionResult Ejercicio6()
+        {
+            return View();
+        }
+
+        public ActionResult Ejercicio7()
+        {
+            return View();
+        }
+
+
+    }
+}
